1e5f86feb457cd28b3e1ab7adae1bfc630552b43
Debashish Sahu <debashish.sahu@gmail.com>
2021-09-21 00:56:12 -0400

    Update links

--- links.js
+++ links.js
@@ -1,12 +1,12 @@
 const links = [	
+	{name: "Web", url: "https://www.debashishsahu.com"},
+	{name: "Scientific Research", url: "https://sites.lsa.umich.edu/debsahu/"}, 
 	{name: "GitHub", url: "https://www.github.com/debsahu"}, 
 	{name: "YouTube", url: "https://www.youtube.com/channel/UC8AWNs6efg2MLhBTuCIJcsg"}, 
 	{name: "Twitter", url: "https://twitter.com/debsahu"},
 	{name: "Instagram", url: "https://www.instagram.com/debsahu"},
-	{name: "RSS", url: "https://www.debashishsahu.com/index.xml"}, 
-	{name: "LinkedIn", url: "https://www.linkedin.com/in/debashish-sahu-69234235/"}, 
-	{name: "Reddit", url: "https://www.reddit.com/user/debsahu/"}, 
-	{name: "Email", url: "mailto:debashish.sahu@gmail.com"}, 
+	{name: "LinkedIn", url: "https://www.linkedin.com/in/debashish-sahu-69234235/"},
+	{name: "BioNMR Core Facility", url: "https://bionmrcore.umich.edu"},
 	{name: "Google Scholar", url: "https://scholar.google.com/citations?user=x_QY8tIAAAAJ&hl=en"},
 ]
 export default links

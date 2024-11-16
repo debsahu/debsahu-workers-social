41b078376bc2b90bd275d3ef8cfade61f71c9775
Debashish Sahu <debashish.sahu@gmail.com>
2024-11-16 02:14:25 +0000

    Add BlueSky

--- links.js
+++ links.js
@@ -3,6 +3,7 @@ const links = [
 	{name: "Scientific Research", url: "https://sites.lsa.umich.edu/debsahu/"}, 
 	{name: "GitHub", url: "https://www.github.com/debsahu"}, 
 	{name: "YouTube", url: "https://www.youtube.com/channel/UC8AWNs6efg2MLhBTuCIJcsg"}, 
+	{name: "BlueSky", url: "https://bsky.app/profile/debsahu.bsky.social"},
 	{name: "Twitter", url: "https://twitter.com/debsahu"},
 	{name: "Instagram", url: "https://www.instagram.com/debsahu"},
 	{name: "LinkedIn", url: "https://www.linkedin.com/in/debashish-sahu-69234235/"},
